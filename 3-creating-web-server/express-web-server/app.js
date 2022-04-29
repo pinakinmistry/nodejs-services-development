ff54ed90ae47a7806a2c7e1846b3ced1497e9d72
Pinakin Mistry <pinakin.nmistry@gmail.com>
2022-04-28 23:10:39 -0400

    Defining routes using express router

--- 3-creating-web-server/express-web-server/app.js
+++ 3-creating-web-server/express-web-server/app.js
@@ -2,5 +2,23 @@
 
 const express = require('express')
 const app = express()
+const createError = require('http-errors')
+const rootRoute = require('./routes/index')
+const helloRoute = require('./routes/hello')
+
+app.use('/', rootRoute)
+app.use('/hello', helloRoute)
+
+app.use((req, res, next) => {
+  if (req.method !== 'GET') {
+    next(createError(405))
+  }
+  next(createError(404))
+})
+
+app.use((err, req, res, next) => {
+  res.status(err.status || 500)
+  res.send(err.message)
+})
 
 module.exports = app
