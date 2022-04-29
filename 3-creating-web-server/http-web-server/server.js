5ed273badcccaae714c216d89e611298f7cd1a36
Pinakin Mistry <pinakin.nmistry@gmail.com>
2022-04-28 22:27:56 -0400

    pathname based response

--- 3-creating-web-server/http-web-server/server.js
+++ 3-creating-web-server/http-web-server/server.js
@@ -2,6 +2,20 @@
 
 const http = require('http');
 const port = process.env.PORT || 3000;
+const url = require('url');
+const { STATUS_CODES } = http;
+
+const root = `<html>
+  <head>
+    <style>
+      body { background: #333; margin: 1.25rem; }
+      h1 { color: #eee; }
+    </style>
+  </head>
+  <body>
+    <a href="/hello">Hello</a1>
+  </body>
+</html>`;
 
 const hello = `<html>
   <head>
@@ -17,7 +31,23 @@ const hello = `<html>
 
 const server = http.createServer((req, res) => {
   res.setHeader('Content-Type', 'text/html');
-  res.end(hello);
+
+  if (req.method !== 'GET') {
+    res.statusCode = 405;
+    res.end(STATUS_CODES[res.statusCode]);
+  }
+
+  const { pathname } = url.parse(req.url);
+
+  if (pathname === '/') {
+    res.end(root);
+  } else if (pathname === '/hello') {
+    res.end(hello);
+  } else {
+    res.statusCode = 404;
+    res.end(STATUS_CODES[res.statusCode]);
+  }
+
 });
 
 server.listen(port);
