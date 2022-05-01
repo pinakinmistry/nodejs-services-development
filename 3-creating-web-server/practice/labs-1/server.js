d2e713c68ce4bdce4cc6cc8ca7cf97b359959904
Pinakin Mistry <pinakin.nmistry@gmail.com>
2022-05-01 08:38:31 -0400

    Handle 404 not found

--- 3-creating-web-server/practice/labs-1/server.js
+++ 3-creating-web-server/practice/labs-1/server.js
@@ -2,8 +2,16 @@
 
 const http = require('http')
 const PORT = process.env.PORT || 3000
+const url = require('url')
+const { STATUS_CODES } = http
 const data = require('./data')
+
 const server = http.createServer(async (req, res) => {
+  const { pathname } = url.parse(req.url)
+  if (pathname !== '/') {
+    res.statusCode = 404
+    res.end(STATUS_CODES[res.statusCode])
+  }
   const value = await data()
   res.end(value)
 })
