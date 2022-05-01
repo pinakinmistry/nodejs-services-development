e7785c55755165cbd8a8823ceb134ab212a0b45e
Pinakin Mistry <pinakin.nmistry@gmail.com>
2022-05-01 09:13:14 -0400

    Handle 405

--- 3-creating-web-server/practice/labs-2/app.js
+++ 3-creating-web-server/practice/labs-2/app.js
@@ -2,9 +2,19 @@
 
 const express = require('express')
 const app = express()
+const createError = require('http-errors')
 
-app.use('/', (req, res) => {
+app.use('/', (req, res, next) => {
+  if (req.method === 'POST') {
+    next(createError(405))
+    return
+  }
   res.send('Valid route')
+})
+
+app.use((err, req, res, next) => {
+  res.status(err.status)
+  res.send(err.message)
 })
 
 module.exports = app
