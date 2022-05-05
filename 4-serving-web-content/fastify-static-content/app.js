40702c3bd4b2e3bf2ee489e3007e8312f91898f2
Pinakin Mistry <pinakin.nmistry@gmail.com>
2022-05-04 22:22:28 -0400

    Serving static content using fastify-static

---
+++ 4-serving-web-content/fastify-static-content/app.js
@@ -0,0 +1,33 @@
+'use strict'
+
+const path = require('path')
+const AutoLoad = require('fastify-autoload')
+const dev = process.env.NODE_ENV !== 'production'
+const fastifyStatic = dev && require('fastify-static')
+
+module.exports = async function (fastify, opts) {
+  // Place here your custom code!
+
+  // Do not touch the following lines
+
+  // This loads all plugins defined in plugins
+  // those should be support plugins that are reused
+  // through your application
+  fastify.register(AutoLoad, {
+    dir: path.join(__dirname, 'plugins'),
+    options: Object.assign({}, opts)
+  })
+
+  // This loads all plugins defined in routes
+  // define your routes in one of these
+  fastify.register(AutoLoad, {
+    dir: path.join(__dirname, 'routes'),
+    options: Object.assign({}, opts)
+  })
+
+  if (dev) {
+    fastify.register(fastifyStatic, {
+      root: path.join(__dirname, 'public')
+    })
+  }
+}
