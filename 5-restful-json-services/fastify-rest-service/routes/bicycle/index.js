b34289c179b0c48d00a0cc92c533f14bee62deb2
Pinakin Mistry <pinakin.nmistry@gmail.com>
2022-05-14 08:18:15 -0400

    Fastify rest service using util.promisify

--- 5-restful-json-services/fastify-rest-service/routes/bicycle/index.js
+++ 5-restful-json-services/fastify-rest-service/routes/bicycle/index.js
@@ -1,16 +1,17 @@
 'use strict'
 
 const { bicycle } = require('../../model')
+const { promisify } = require('util')
+const read = promisify(bicycle.read)
 
 module.exports = async (fastify, opt) => {
   fastify.get('/:id', async (request, reply) => {
     const { id } = request.params
-    bicycle.read(id, (err, result) => {
-      if (err) {
-        if (err.message === 'not found') reply.notFound()
-        else reply.send(err)
-      } else reply.send(result)
-    })
-    await reply
+    try {
+      return await read(id)
+    } catch(err) {
+      if (err.message === 'not found') reply.notFound()
+      else reply.send(err)
+    }
   })
 }
