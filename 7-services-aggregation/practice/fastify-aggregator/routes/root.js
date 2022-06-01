55ff032f18851f10fa898070464e4609d46053cb
Pinakin Mistry <pinakin.nmistry@gmail.com>
2022-06-01 17:46:52 -0400

    Fetching data using got

--- 7-services-aggregation/practice/fastify-aggregator/routes/root.js
+++ 7-services-aggregation/practice/fastify-aggregator/routes/root.js
@@ -1,15 +1,21 @@
 'use strict'
 
+const got = require('got')
+
 module.exports = async function (fastify, opts) {
   const {
     BOAT_SERVICE_PORT,
     BRAND_SERVICE_PORT
   } = process.env
 
-  fastify.get('/', async function (request, reply) {
-    console.log(BOAT_SERVICE_PORT, BRAND_SERVICE_PORT)
+  fastify.get('/:id', async function (request, reply) {
+    const {id} = request.params
+    const boat = await got(`http://localhost:${BOAT_SERVICE_PORT}/${id}`).json()
+    const brand = await got(`http://localhost:${BRAND_SERVICE_PORT}/${boat.brand}`).json()
     reply.send({
-      BOAT_SERVICE_PORT, BRAND_SERVICE_PORT
+      id: boat.id,
+      color: boat.color,
+      brand: brand.name
     })
   })
 }
