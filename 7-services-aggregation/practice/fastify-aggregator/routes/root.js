15dab068809b92166ca470c850c8302baae820d0
Pinakin Mistry <pinakin.nmistry@gmail.com>
2022-06-01 21:19:57 -0400

    node validate in progress

--- 7-services-aggregation/practice/fastify-aggregator/routes/root.js
+++ 7-services-aggregation/practice/fastify-aggregator/routes/root.js
@@ -7,15 +7,23 @@ module.exports = async function (fastify, opts) {
     BOAT_SERVICE_PORT,
     BRAND_SERVICE_PORT
   } = process.env
+  const {notFound, badRequest} = fastify.httpErrors
 
   fastify.get('/:id', async function (request, reply) {
     const {id} = request.params
-    const boat = await got(`http://localhost:${BOAT_SERVICE_PORT}/${id}`).json()
-    const brand = await got(`http://localhost:${BRAND_SERVICE_PORT}/${boat.brand}`).json()
-    reply.send({
-      id: boat.id,
-      color: boat.color,
-      brand: brand.name
-    })
+    try {
+      const boat = await got(`http://localhost:${BOAT_SERVICE_PORT}/${id}`).json()
+      const brand = await got(`http://localhost:${BRAND_SERVICE_PORT}/${boat.brand}`).json()
+      reply.send({
+        id: boat.id,
+        color: boat.color,
+        brand: brand.name
+      })
+    } catch (err) {
+      if (!err.response) throw err
+      else if (err.response.statusCode === 404) throw notFound()
+      else if (err.response.statusCode === 400) throw badRequest()
+      else throw err
+    }
   })
 }
