6460ca88f3f1dd7ce95c9a14e8a0fa84b89df17a
Pinakin Mistry <pinakin.nmistry@gmail.com>
2022-06-01 22:34:33 -0400

    Timeout logic: to be added

---
+++ 7-services-aggregation/practice/labs-1/routes/root.js
@@ -0,0 +1,33 @@
+'use strict'
+
+const got = require('got')
+
+module.exports = async function (fastify, opts) {
+  const {
+    BOAT_SERVICE_PORT,
+    BRAND_SERVICE_PORT
+  } = process.env
+  const {notFound, badRequest} = fastify.httpErrors
+
+  fastify.get('/:id', async function (request, reply) {
+    const {id} = request.params
+    try {
+      const boat = await got(`http://localhost:${BOAT_SERVICE_PORT}/${id}`).json()
+      const brand = await got(`http://localhost:${BRAND_SERVICE_PORT}/${boat.brand}`).json()
+      reply.send({
+        id: boat.id,
+        color: boat.color,
+        brand: brand.name
+      })
+    } catch (err) {
+      if (!err.response) throw err
+      if (err.response.statusCode === 404) {
+        throw notFound()
+      }
+      if (err.response.statusCode === 400) {
+        throw badRequest()
+      }
+      throw err
+    }
+  })
+}
