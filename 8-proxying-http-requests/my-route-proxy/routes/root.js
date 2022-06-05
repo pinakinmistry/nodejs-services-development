d4a697d33e44c1880bf493d02e7de44f524ecbee
Pinakin Mistry <pinakin.nmistry@gmail.com>
2022-06-05 08:14:34 -0400

    Single route, multiple origin proxy using fastify-reply-from package

---
+++ 8-proxying-http-requests/my-route-proxy/routes/root.js
@@ -0,0 +1,14 @@
+'use strict'
+
+module.exports = async function (fastify, opts) {
+  const {badRequest} = fastify.httpErrors
+  fastify.get('/', async function (request, reply) {
+    const {url} = request.query
+    try {
+      new URL(url)
+    } catch (err) {
+      throw badRequest()
+    }
+    return reply.from(url)
+  })
+}
