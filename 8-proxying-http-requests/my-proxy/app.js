8be85b36629fb24e982b366ee5e9ad49dd33fbec
Pinakin Mistry <pinakin.nmistry@gmail.com>
2022-06-05 10:40:06 -0400

    Single origin multiple route with fastify-http-proxy

---
+++ 8-proxying-http-requests/my-proxy/app.js
@@ -0,0 +1,9 @@
+'use strict'
+
+const proxy = require('fastify-http-proxy')
+
+module.exports = async function (fastify, opts) {
+  fastify.register(proxy, {
+    upstream: 'htt‌ps://news.ycombinator.com/'
+  })
+}
